7bc1574843b20e46ebc3dac4ff21776e581b4526
Jie你的账 <2805889238@qq.com>
2023-07-11 14:24:28 +0800

    完成了商品列表页面的开发

---
+++ src/components/my-goods/my-goods.vue
@@ -0,0 +1,73 @@
+<!--
+ * @Author: Jie你的账 2805889238@qq.com你邮箱地
+ * @Date: 2023-06-27 21:21:48
+ * @LastEditors: Jie你的账 2805889238@qq.com你邮箱地
+ * @LastEditTime: 2023-06-29 00:07:58
+ * @FilePath: \uni-shop-1\src\components\my-goods\my-goods.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <view class="goods-item">
+          <!-- 左侧的盒子 -->
+          <view class="goods-item-left">
+            <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+          </view>
+          <!-- 右侧的盒子 -->
+          <view class="goods-item-right">
+            <!-- 商品的名字 -->
+            <view class="goods-name">{{ goods.goods_name }}</view>
+            <view class="goods-info-box">
+              <!-- 商品价格 -->
+              <view class="goods-price">¥{{ parseFloat(goods.goods_price).toFixed(2) }}</view>
+            </view>
+          </view>
+       </view>
+</template>
+
+<script>
+  export default {
+    props: {
+      goods: {
+        type: Object,
+        default: {}
+      }
+    },
+    data() {
+      return {
+        // 默认图片地址
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      }
+    }
+}
+
+</script>
+
+<style lang="scss">
+.goods-item {
+  display: flex;
+  padding: 10px 5px;
+  border-bottom: 1px solid #f0f0f0;
+  .goods-item-left{
+    margin-right: 5px;
+    .goods-pic {
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+  }
+  .goods-item-right{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .goods-name {
+      font-size: 13px;
+    }
+    .goods-info-box {
+      .goods-price {
+        color: #C00000;
+        font-size: 16px;
+      }
+    }
+  }
+}
+</style>
--- src/subpkg/goods_list/goods_list.vue
+++ src/subpkg/goods_list/goods_list.vue
@@ -1,8 +1,88 @@
+<!--
+ * @Author: Jie你的账 2805889238@qq.com你邮箱地
+ * @Date: 2023-06-23 20:18:10
+ * @LastEditors: Jie你的账 2805889238@qq.com你邮箱地
+ * @LastEditTime: 2023-07-11 14:14:21
+ * @FilePath: \uni-shop-1\src\subpkg\goods_list\goods_list.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <div class="goods_list">goods_list</div>
+  <view class="goods-list">
+    <view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+       <my-goods :goods="goods"></my-goods>
+    </view>
+  </view>
 </template>
 
-<script setup>
+<script>
+  export default {
+    data() {
+      return {
+        // 请求参数对象
+        queryObj:{
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        goodsList: [],
+        total:0,
+        isloading: true
+      };
+    },
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+
+      this.getGoodsList()
+    },
+    methods: {
+      async getGoodsList (cb) {
+        // 打开节流阀
+        this.isloading = true
+       const {data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+       //请求失败
+       if(res.meta.status !==200) return uni.$showMsg() 
+
+       this.goodsList = [...this.goodsList,...res.message.goods]
+       this.total = res.message.total
+       // 关闭节流阀
+       this.isloading = false
+       cb && cb()
+      },
+      gotoDetail(goods) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+        })
+      }
+    },
+    //页面上拉触底事件的处理函数
+    onReachBottom() {
+      // 当前的页码值 * 每页显示多少条数据 >= 总数条数
+      // pagenum * pagesize >= this.total
+
+      // 判断是否还有下一页数据
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+
+      // 判断是否正在请求其他数据，如果是，则不发起额外请求
+      if(this.isloading) return
+      // 让页码值自增 +1
+      this.queryObj.pagenum++
+      this.getGoodsList
+    },
+    onPullDownRefresh() {
+      // 重置关键数据
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isloading = false
+      this.goodsList = []
+
+      // 重新发起数据请求
+      this.getGoodsList(() => uni.stopPullDownRefresh())
+    }
+  }
 </script>
 
-<style scoped></style>
+<style lang="scss">
+
+</style>
